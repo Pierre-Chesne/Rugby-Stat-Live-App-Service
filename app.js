f43836075b91cbe7ddba40a1d888d33aab115384
Pierre-Chesne <pierrc@hotmail.com>
2021-10-18 12:12:33 +0200

    Image

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const cors = require('cors');
-const index = require('./routes/index');
+const mysql = require ('mysql');
+require('dotenv').config();
+
 
 app = express();
 app.use(cors());
+app.use(express.static('public'));
 
 
-// Routes
-app.use('/', index);
+// Info conexion DB
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_MANE
+});
 
+// Connexion à la base
+connection.connect(error => {
+    if (error) throw error;
+});
 
 
 app.listen(5500)
