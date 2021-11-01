33719a71be5727570c3cacfd742dacbc382a022f
Pierre-Chesne <pierrc@hotmail.com>
2021-11-01 11:42:47 +0100

    finish test

---
+++ MySQL/create_base.sql
@@ -0,0 +1,26 @@
+create table plaquage_ok (
+    Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sum_plaquage_ok int
+);
+create table plaquage_ko (
+    Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sum_plaquage_ko int
+);
+create table melee_ok (
+    Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sum_melee_ok int
+);
+create table melee_ko (
+    Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sum_melee_ko int
+);
+create table touche_ok (
+    Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sum_touche_ok int
+);
+create table touche_ko (
+    Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    sum_touche_ko int
+)
+
+
--- app.js
+++ app.js
@@ -21,7 +21,6 @@ connection.connect(error => {
     if (error) throw error;
 });
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Plaquage OK
 app.get('/plaquage-ok/get',(req,res) => {
     connection.query('select * from plaquage_ok' , (error,results) => {
@@ -34,10 +33,7 @@ app.get('/plaquage-ok/get',(req,res) => {
 app.post ('/plaquage-ok/add', (req,res) => {
     let postData = req.body;
     connection.query('UPDATE plaquage_ok SET sum_plaquage_ok=sum_plaquage_ok+1 WHERE Id = 1' , postData,(error, results,fields) => {
-        if (error) throw error;
-        //res.end(JSON.stringify(results));
-        //res.end();
-        //res.sendfile('./public/plaquage.html')        
+        if (error) throw error;   
     });
 });
 
@@ -45,14 +41,10 @@ app.post ('/plaquage-ok/add', (req,res) => {
 app.post ('/plaquage-ok/reset', (req,res) => {
     let postData = req.body;
     connection.query('UPDATE plaquage_ok SET sum_plaquage_ok = 0 where id = 1' , postData,(error, results,fields) => {
-        if (error) throw error;
-        //res.end(JSON.stringify(results));
-        //res.end();
-        //res.sendfile('./public/plaquage.html')        
+        if (error) throw error;     
     });
 });
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Plaquage KO
 app.get('/plaquage-ko/get',(req,res) => {
     connection.query('select * from plaquage_ko' , (error,results) => {
@@ -65,10 +57,7 @@ app.get('/plaquage-ko/get',(req,res) => {
 app.post ('/plaquage-ko/add', (req,res) => {
     let postData = req.body;
     connection.query('UPDATE plaquage_ko SET sum_plaquage_ko=sum_plaquage_ko+1 WHERE Id = 1' , postData,(error, results,fields) => {
-        if (error) throw error;
-        //res.end(JSON.stringify(results));
-        //res.end();
-        //res.sendfile('./public/plaquage.html')        
+        if (error) throw error;     
     });
 });
 
@@ -76,11 +65,105 @@ app.post ('/plaquage-ko/add', (req,res) => {
 app.post ('/plaquage-ko/reset', (req,res) => {
     let postData = req.body;
     connection.query('UPDATE plaquage_ko SET sum_plaquage_ko = 0 where id = 1' , postData,(error, results,fields) => {
-        if (error) throw error;
-        //res.end(JSON.stringify(results));
-        //res.end();
-        //res.sendfile('./public/plaquage.html')        
+        if (error) throw error;      
     });
 });
+
+// Mêlée OK
+app.get('/melee-ok/get',(req,res) => {
+    connection.query('select * from melee_ok' , (error,results) => {
+        if(error) throw error;
+        res.end(JSON.stringify({results}));
+    })
+});
+
+// Incremente Mêlée OK
+app.post ('/melee-ok/add', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE melee_ok SET sum_melee_ok=sum_melee_ok+1 WHERE Id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;        
+    });
+});
+
+// reset Mêlées OK
+app.post ('/melee-ok/reset', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE melee_ok SET sum_melee_ok = 0 where id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;      
+    });
+});
+
+// Mêlée KO
+app.get('/melee-ko/get',(req,res) => {
+    connection.query('select * from melee_ko' , (error,results) => {
+        if(error) throw error;
+        res.end(JSON.stringify({results}));
+    })
+});
+
+// Incremente Mêlée KO
+app.post ('/melee-ko/add', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE melee_ko SET sum_melee_ko=sum_melee_ko+1 WHERE Id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;        
+    });
+});
+
+// Reset Mêlée KO
+app.post ('/melee-ko/reset', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE melee_ko SET sum_melee_ko = 0 where id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;   
+    });
+});
+
+// Touche OK
+app.get('/touche-ok/get',(req,res) => {
+    connection.query('select * from touche_ok' , (error,results) => {
+        if(error) throw error;
+        res.end(JSON.stringify({results}));
+    })
+});
+
+// Incremente Touche OK
+app.post ('/touche-ok/add', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE touche_ok SET sum_touche_ok=sum_touche_ok+1 WHERE Id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;        
+    });
+});
+
+// Reset Touches OK
+app.post ('/touche-ok/reset', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE touche_ok SET sum_touche_ok= 0 where id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;   
+    });
+});
+
+// Touche KO
+app.get('/touche-ko/get',(req,res) => {
+    connection.query('select * from touche_ko' , (error,results) => {
+        if(error) throw error;
+        res.end(JSON.stringify({results}));
+    })
+});
+
+// Incremente Touche KO
+app.post ('/touche-ko/add', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE touche_ko SET sum_touche_ko=sum_touche_ko+1 WHERE Id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;        
+    });
+});
+
+// Reset Touches KO
+app.post ('/touche-ko/reset', (req,res) => {
+    let postData = req.body;
+    connection.query('UPDATE touche_ko SET sum_touche_ko= 0 where id = 1' , postData,(error, results,fields) => {
+        if (error) throw error;   
+    });
+});
+
 
 app.listen(5500)
